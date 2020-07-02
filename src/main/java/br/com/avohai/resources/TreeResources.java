efdab2af4e370a08524b91dd182d4d22e87dae95
conrado <conradocjo@gmail.com>
2020-07-01 23:06:15 -0300

    Ajuste do serviço teste

--- src/main/java/br/com/avohai/resources/TreeResources.java
+++ src/main/java/br/com/avohai/resources/TreeResources.java
@@ -1,5 +1,6 @@
 package br.com.avohai.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tree")
 public class TreeResources {
 
-	@RequestMapping(value = "/teste", method = RequestMethod.GET)
+	@GetMapping(value = "/teste")
 	public String testeConrado() {
 		return "Tudo funcionando!";
 	}
+
 }
